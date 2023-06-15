a161f5e6ce211a0d44bb9124b0ca6f053fd00867
NicoleKobs <nicole.kobs@deutschebahn.com>
2023-06-16 00:25:17 +0200

    - added another base in config - added wildcard route to redirect

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -4,8 +4,17 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 export const appRoutes: Route[] = [
   {
     path: 'memory',
+    title: 'Angular Memory Game',
     loadChildren: () =>
       import('@apo/memory-game').then((m) => m.memoryGameRoutes),
   },
-  { path: '', component: DashboardComponent },
+  {
+    path: '',
+    title: 'Apokryph Angular Games Dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
 ];
